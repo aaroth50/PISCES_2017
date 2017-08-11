ca6d55dedd60e4f259b71c10d83d368bf2e72bc2
aaroth50 <aaronkroth@yahoo.com>
2017-08-10 15:34:19 -1000

    Added additive blending to anaglyph code

--- anaglyph.py
+++ anaglyph.py
@@ -12,11 +12,29 @@ leftMap = left.load()
 
 right = grayscale(right)
 right = colorize(right, (0,0,0),(0,255,255)) #cyan (0,255,255)
+#right = colorize(right, (0,0,0),(0,0,255)) #blue (0,0,255)
 
 left = grayscale(left)
 left = colorize(left, (0,0,0),(255,0,0)) #red (255,0,0)
 
+'''
+#This code for blend
 o = ImageChops.add(right,left,2)
 
 o.save("3Dpics/anaglyph.jpg", "JPEG")
+'''
 
+#This code for additive blending
+list_out = []
+list_out1 = []
+list_out2 = []
+for red, cyan in itertools.izip(list(left.getdata()), list(right.getdata())):
+	list_out.append(min(red[0], 255))
+	list_out1.append(min(cyan[1], 255))
+	list_out2.append(min(cyan[2], 255))
+
+image_out = Image.new(right.mode,left.size)
+combo = zip(list_out,list_out1,list_out2)
+image_out.putdata(combo)
+
+image_out.save("3Dpics/anaglyph.jpg", "JPEG")
